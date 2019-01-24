ff4e816b00738d99ee2bbb74387028136c44e65b
santiago <sjglp97@gmail.com>
2019-01-24 20:28:45 +0100

    first commit

---
+++ BolaGame/Assets/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour{
+
+    private Rigidbody rb;
+    public float speed;
+    public float speedJump;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV)*speed;
+        rb.AddForce(movement);
+
+         if (Input.GetKeyDown(KeyCode.Space))
+          {
+              GetComponent<Rigidbody>().velocity = Vector3.up * speedJump;
+                            
+          }
+    }
+}
